d312fb83bef51235ee9275bc69b1c4031c85a988
Mohammed Yasser <2100921@eng.asu.edu.eg>
2024-12-22 19:30:43 +0200

    Add support for running a DHCP LAN server via command line argument

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import argparse
 from dhcp.dhcp_client import Client
 from dhcp.dhcp_server import Server
+from dhcp.LAN_dhcp_server import LAN_Server
 
 
 # Initialize the argument parser
 parser = argparse.ArgumentParser(description="Run a DHCP server or client")
 parser.add_argument('--server', action='store_true', help="Run the DHCP server")
 parser.add_argument('--client', action='store_true', help="Run the DHCP client")
+parser.add_argument('--LAN', action='store_true', help="Run the DHCP LAN server")
 
 # Parse the arguments
 args = parser.parse_args()
@@ -16,6 +18,8 @@ if args.server:
     Server.start_server()
 elif args.client:
     Client.start_client()
+elif args.LAN:
+    LAN_Server.start_server()
 else:
     print("You must specify either --server or --client")
 

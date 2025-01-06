efa9910498b26eb6eb39b7192e1155f9edf03d8c
Ahmed-Nezar <ahmednezarrr@gmail.com>
2025-01-06 16:04:18 +0200

    Add freeze_support for multiprocessing compatibility in main GUI server

--- main_gui_server.py
+++ main_gui_server.py
@@ -1,7 +1,10 @@
 from gui.gui_server import DHCPServerGUI
 import customtkinter as ctk
+from multiprocessing import freeze_support
+
 
 if __name__ == "__main__":
+    freeze_support()
     root = ctk.CTk()
     DHCPServerGUI(root)
     root.geometry("800x600")
